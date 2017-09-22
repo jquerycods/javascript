777da9ba22572905447ce3b4eaa66fb1c8efa8c0
jquerycods <cizgifilmhikayeleri@gmail.com>
2017-09-22 12:27:04 +0300

    Delete disqus.name.js

--- disqus.name.js
+++
@@ -1,9 +0,0 @@
-﻿(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/tr_TR/sdk.js#xfbml=1&version=v2.8";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
-$(document).ready(function() { var disqus_shortname = 'bloggerekibi'; (function() { var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true; dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js'; (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })(); });
